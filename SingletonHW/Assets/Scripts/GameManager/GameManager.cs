8cf93d048a96df3aa7e77a0974aaf67d758ae171
LebronzoCodes <alebronsfs@gmail.com>
2024-10-02 12:24:48 -0400

    NearlyMessedItUp

--- SingletonHW/Assets/Scripts/GameManager/GameManager.cs
+++ SingletonHW/Assets/Scripts/GameManager/GameManager.cs
@@ -12,10 +12,6 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance;
 
-    public TextMeshProUGUI score;
-    public TextMeshProUGUI p3score;
-    public int sc_num;
-    public int p3sc_num;
     public bool sceneChanged = false;
 
     // Awake is called on spawn of this script before start
@@ -33,29 +29,22 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
         
 
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        //Make the score 0
-        sc_num = 0;
-        p3sc_num = 0;
-
-
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        score.text = "Score: " + sc_num;
-        p3score.text = "Score: " + p3sc_num;
+       
 
     }
 
     //Create Function to load scenes
-    public void StartGame()
+    public void ChangeScene()
     {
         //Either one will lead to game scene. 
         //SceneManager.LoadScene("Game Scene");
--- SingletonHW/Assets/Scripts/WASD/WASD_rb.cs
+++ SingletonHW/Assets/Scripts/WASD/WASD_rb.cs
@@ -64,7 +64,7 @@ public class WASD_rb : MonoBehaviour
         if (GameManager.instance.sceneChanged == false)
         {
             GameManager.instance.sceneChanged = true;
-            GameManager.instance.StartGame();
+            GameManager.instance.ChangeScene();
         }
        
         
